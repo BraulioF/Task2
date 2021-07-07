9cf3ff4ea92d87545738c4c72796b02fb05d3daa
BraulioF <bfernandez@adnetworks.cl>
2021-07-07 12:35:19 -0400

    Subiendo cambios en Auth

--- models/auth/odoo.py
+++ models/auth/odoo.py
@@ -2,23 +2,18 @@
 
 import xmlrpc.client
 
-
-import xmlrpc.client
-
 url = 'http://52.142.63.20:1269'
 db = 'fraccion_test'
 username = 'WSadnet@adnetworks.cl'
 password = 'WSadnet@adnetworks.cl'
 
 class OdooClient():
-
-   
+  
     def __init__(self):
         self.url = url
         self.db = db
         self.username = username
         self.password = password
-
 
     def logging(self):
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
