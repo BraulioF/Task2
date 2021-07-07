f268902a701ba4e9e98166009a8992d176b36906
BraulioF <bfernandez@adnetworks.cl>
2021-07-07 18:19:43 -0400

    cambios finales

--- README.md
+++ README.md
@@ -10,12 +10,14 @@ ADNETWORKS WEB SERVICE
 * ubuntu
 
 # routes
-
+127.0.0.1:5000/partner
+127.0.0.1:5000/partner/create
 * **auth**
 
 # entorno de desarrollo
-pip insta
-
+sudo pip3 install virtualenv
+pip install virtualenv
+pip install flask
 # usage
 
 # information
--- api.py
+++ api.py
@@ -7,7 +7,6 @@ from models import odoo
 import datetime
 import time
 import config as cg
-import logging
 
 #Global varial
 HOST = cg.server['host']
@@ -19,16 +18,47 @@ app = Flask(__name__)
 #Views parnter list
 @app.route("/partner", methods=["GET"])
 def search_read():
-    parnters = rs_partner.ResPartnerList.res_partner()
-    return jsonify(parnters)
+#    parnters = rs_partner.ResPartnerList.res_partner()
+#    return jsonify(parnters)
+    partners = rs_partner.ResPartnerList.ObtenerPartnerSegunID()
+    return jsonify(partners)
 
 #Views parnter list
 @app.route("/partner/create", methods=["POST"])
 #Enviar a la ruta a traves de postman un json con el name el phone y el email
-def partner_create():
+def create():
     #capturo json
+    name = request.json['name']
+    rut = request.json['rut']
+    comment = request.json['comment']
+    phone = request.json['phone']
+    email = request.json['email']
+    crear = rs_partner.ResPartnerList.partner_create(name,rut,comment,phone,email)
     #y lo mando a su resource
-    return jsonify(parnters)
+    return jsonify({'Creado':crear})
+
+@app.route("/partner/<id>", methods=["PUT"])
+def update_partner(id):
+
+    partners = rs_partner.ResPartnerList.ObtenerPartnerSegunID(id)
+    if(len(partners)== 0):
+        return "Ese ID no existe"
+    else:
+        #print(partners)
+        data = request.get_json()
+        rs_partner.ResPartnerList.ActualizarPartnerSegunID(id,data)
+        return jsonify(partners)
+
+@app.route("/partner/drop/<id>", methods=["DELETE"])
+def drop_partner(id):    
+    partners = rs_partner.ResPartnerList.ObtenerPartnerSegunID(id)
+    if(len(partners)== 0):
+        return "Ese ID no existe"
+    else:
+        #print(partners)
+        #data = request.get_json()
+        verificar =rs_partner.ResPartnerList.EliminarSegunID(id)
+        return jsonify(verificar)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
